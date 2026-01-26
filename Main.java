ff0f9b6aada4494274de26c954507e34fb5d3b30
NarumiOmukai-ctrl <narumi.omukai@gmail.com>
2026-01-26 18:55:04 +0900

    updated main file

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+class Car {
+    String color;
+    int speed;
+
+    // --- コンストラクタ ---
+    // クラス名(Car)と同じ。戻り値なし。
+    Car(String c) {
+        color = c;    // インスタンス化時の初期設定
+        speed = 0;    // 初速は0
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        // newした瞬間にコンストラクタが動く
+        Car myCar = new Car("赤");
+        System.out.println(myCar.color); // 出力: 赤
     }
 }
